96a853a79d3744302538bd0259da87fbbc7ddf4e
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-09-09 19:02:52 +0300

    Provide a generated sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+import prisma from "../db";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const pages: Array<{ url: string, lastModified: Date }> = [];
+
+  const companies = await prisma.company.findMany();
+
+  pages.push(...companies.map((company) => ({
+    url: `${process.env.PUBLIC_URL}/companies/${company.id}`,
+    lastModified: company.updatedAt,
+  })));
+
+  const posts = await prisma.post.findMany();
+
+  pages.push(...posts.map((post) => ({
+    url: `${process.env.PUBLIC_URL}/posts/${post.id}`,
+    lastModified: post.updatedAt,
+  })));
+
+  return pages;
+}
