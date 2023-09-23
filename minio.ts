0b131973201020bece4aac5f8097499fde876e03
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-09-23 14:38:09 +0300

    Log MinIO connection information

--- minio.ts
+++ minio.ts
@@ -3,10 +3,16 @@ import * as Minio from 'minio';
 const createMinioSingleton = () => {
   const minioUrl = new URL(process.env.MINIO_URL!);
 
+  const endPoint = minioUrl.hostname;
+  const port = parseInt(minioUrl.port, 10);
+  const useSSL = minioUrl.protocol === 'https';
+
+  console.log(`Connecting to MinIO: endPoint=${JSON.stringify(endPoint)} port=${JSON.stringify(port)} useSSL=${JSON.stringify(useSSL)}`);
+
   return new Minio.Client({
-    endPoint: minioUrl.hostname,
-    port: parseInt(minioUrl.port, 10),
-    useSSL: minioUrl.protocol === 'https',
+    endPoint,
+    port,
+    useSSL,
     accessKey: process.env.MINIO_ACCESS_KEY!,
     secretKey: process.env.MINIO_SECRET_KEY!,
   });
