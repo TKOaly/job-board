09660b0f8513751ee55cca97d10f0349398b0888
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-12-23 20:54:43 +0200

    Implement LanguageSelector component

---
+++ components/LanguageSelector.tsx
@@ -0,0 +1,30 @@
+import { LanguageIcon } from "@heroicons/react/24/solid";
+import { useTranslation } from "@/app/i18n/client";
+import { useParams, useRouter } from "next/navigation";
+import { useSelectedLayoutSegments } from "next/navigation";
+import { fallbackLang } from "@/app/i18n/settings";
+
+export const LanguageSelector = () => {
+  const segments = useSelectedLayoutSegments();
+  const router = useRouter();
+  const params = useParams();
+  const language = params?.lang ?? fallbackLang;
+
+  const options = [
+    { key: 'fi', label: 'Suomi' },
+    { key: 'en', label: 'English' },
+  ]
+
+  const { label, key } = options[(options.findIndex(({ key }) => key === language) + 1) % options.length];
+
+  const onChange = () => {
+    router.push(`/${key}/${segments.join('/')}`)
+  };
+
+  return (
+    <div className="flex items-center gap-2 hover:bg-black/10 p-2 rounded cursor-pointer" onClick={onChange}>
+      <LanguageIcon className="h-5 w-5" />{' '}
+      <span className="hidden md:inline">{label}</span>
+    </div>
+  );
+};
