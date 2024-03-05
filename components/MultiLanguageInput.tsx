3a1576c9c50d507f450cc5f3100ad361d5b0056b
Eetu Raunio <eetu.raunio@helsinki.fi>
2024-03-06 00:29:35 +0200

    Focus MultiLanguageInput input when selecting language

--- components/MultiLanguageInput.tsx
+++ components/MultiLanguageInput.tsx
@@ -1,19 +1,47 @@
-import { autoUpdate, offset, shift, useDismiss, useFloating, useInteractions } from '@floating-ui/react';
+import {
+  autoUpdate,
+  offset,
+  shift,
+  useDismiss,
+  useFloating,
+  useInteractions,
+} from '@floating-ui/react';
 import { produce } from 'immer';
-import { ChangeEventHandler, useState } from 'react';
+import {
+  ChangeEventHandler,
+  MutableRefObject,
+  RefCallback,
+  useRef,
+  useState,
+} from 'react';
 import { Input } from './Input';
 
 export type Props = {
-  component?: React.JSXElementConstructor<any>
-  value?: Record<string, string>,
-  onValueChange?: (value: Record<string, string>) => void,
-  open?: boolean,
-}
+  component?: React.JSXElementConstructor<any>;
+  value?: Record<string, string>;
+  onValueChange?: (value: Record<string, string>) => void;
+  open?: boolean;
+};
 
 const LANGUAGES = {
   xx: 'Unknown',
   fi: 'Finnish',
   en: 'English',
+};
+
+// https://phuoc.ng/collection/react-ref/merge-different-refs/
+function mergeRefs<T>(
+  refs: Array<MutableRefObject<T> | React.LegacyRef<T> | null>,
+): RefCallback<T> {
+  return value => {
+    refs.forEach(ref => {
+      if (typeof ref === 'function') {
+        ref(value);
+      } else if (ref != null) {
+        (ref as React.MutableRefObject<T | null>).current = value;
+      }
+    });
+  };
 }
 
 export const MultiLanguageInput = ({
@@ -23,21 +51,30 @@ export const MultiLanguageInput = ({
   open: pOpen = false,
 }: Props) => {
   const value = pValue
-    ? Object.fromEntries(Object.entries(pValue).filter(([key, value]) => value && value.length > 0))
+    ? Object.fromEntries(
+        Object.entries(pValue).filter(
+          ([key, value]) => value && value.length > 0,
+        ),
+      )
     : {};
 
+  const inputRef = useRef<HTMLInputElement>(null);
   const [open, setOpen] = useState(pOpen);
   const [language, setLanguage] = useState(Object.keys(value)[0] ?? 'xx');
+  const setSelectedLanguage = (lang: string) => {
+    setLanguage(lang);
+    inputRef.current?.focus();
+  };
 
-  const languageValue = value?.[language] ?? ''; 
+  const languageValue = value?.[language] ?? '';
 
   const setLanguageValue = (langValue: string) => {
-    const newValue = produce(value ?? {}, (value) => {
+    const newValue = produce(value ?? {}, value => {
       value[language] = langValue;
     });
 
     onValueChange?.(newValue);
-  }
+  };
 
   const { refs, floatingStyles, context } = useFloating({
     open,
@@ -55,17 +92,19 @@ export const MultiLanguageInput = ({
   });
 
   const { getReferenceProps, getFloatingProps } = useInteractions([
-    useDismiss(context, { })
-  ])
+    useDismiss(context, {}),
+  ]);
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (evt) => {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = evt => {
     setLanguageValue(evt.target.value);
   };
+
+  const mergedInputRef = mergeRefs([inputRef, refs.setReference]);
 
   return (
     <div>
       <InputComponent
-        ref={refs.setReference}
+        ref={mergedInputRef}
         value={languageValue}
         onChange={handleChange}
         {...getReferenceProps({
@@ -77,16 +116,28 @@ export const MultiLanguageInput = ({
           ref={refs.setFloating}
           style={floatingStyles}
           className="px-1 text-sm rounded-full border shadow-sm flex divide-x bg-white"
-          {...getFloatingProps({})}>
-          {
-            Object.entries(LANGUAGES)
-              .filter(([key]) => key !== 'xx' || Object.values(value ?? {}).every((v) => v.length === 0) || value?.xx !== undefined)
-              .map(([key, label]) => (
-                <div className={`py-1 px-2 cursor-pointer ${key === language ? 'font-semibold' : ''}`} onClick={() => setLanguage(key)}>{label}</div>
-              ))
-          }
+          {...getFloatingProps({})}
+        >
+          {Object.entries(LANGUAGES)
+            .filter(
+              ([key]) =>
+                key !== 'xx' ||
+                Object.values(value ?? {}).every(v => v.length === 0) ||
+                value?.xx !== undefined,
+            )
+            .map(([key, label]) => (
+              <div
+                key={key}
+                className={`py-1 px-2 cursor-pointer ${
+                  key === language ? 'font-semibold' : ''
+                }`}
+                onClick={() => setSelectedLanguage(key)}
+              >
+                {label}
+              </div>
+            ))}
         </div>
       )}
     </div>
   );
-}
+};
